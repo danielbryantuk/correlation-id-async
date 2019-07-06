9f7a875f687b228989edadf2be02b25abe7a7095
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-07-06 16:35:10 +0200

    Update dependency versions

--- src/main/java/uk/co/taidev/experiments/correlationidasync/Application.java
+++ src/main/java/uk/co/taidev/experiments/correlationidasync/Application.java
@@ -2,7 +2,7 @@ package uk.co.taidev.experiments.correlationidasync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
